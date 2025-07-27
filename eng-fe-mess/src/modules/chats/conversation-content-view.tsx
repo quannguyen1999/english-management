d508c5827cdfc09b5978fcac0b9dc387d8e78829
quannguyen1999 <nguyendanganhquan99@gmail.com>
2025-07-27 23:16:50 +0700

    fix conversations

--- eng-fe-mess/src/modules/chats/conversation-content-view.tsx
+++ eng-fe-mess/src/modules/chats/conversation-content-view.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { useConversation } from "@/components/providers/conversation-provider";
+import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { SmileIcon } from "lucide-react";
+import { useEffect, useRef } from "react";
 export default function ConversationContentView() {
   const { messages } = useConversation();
+  const endOfMessagesRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    endOfMessagesRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <ScrollArea className="h-full pb-2">
       {messages.map((item: any) => (
@@ -13,14 +22,26 @@ export default function ConversationContentView() {
             item.sender === "John Doe" ? "justify-end" : "justify-start"
           } space-x-2`}
         >
-          <div className="flex flex-col max-w-[70%] gap-y-2">
+          <div className="flex flex-col max-w-[70%] gap-y-2 relative">
             <p className="text-sm text-gray-500">{item.sender}</p>
             <p className="bg-gray-200 dark:bg-gray-800 p-2 rounded-lg">
               {item.message}
             </p>
+
+            <Button
+              className={
+                item.sender === "John Doe"
+                  ? "absolute -bottom-4 -left-4 text-2xl"
+                  : "absolute -bottom-4 -right-4 text-2xl"
+              }
+              variant="icon"
+            >
+              <SmileIcon />
+            </Button>
           </div>
         </div>
       ))}
+      <div ref={endOfMessagesRef} />
     </ScrollArea>
   );
 }
--- eng-fe-mess/src/modules/chats/conversation-input-view.tsx
+++ eng-fe-mess/src/modules/chats/conversation-input-view.tsx
@@ -1,12 +1,28 @@
 "use client";
 
 import { useConversation } from "@/components/providers/conversation-provider";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
+import EmojiPicker from "emoji-picker-react";
 import { MicIcon, SendIcon, SmileIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function ConversationInputView() {
   const [message, setMessage] = useState("");
+  const [showPicker, setShowPicker] = useState(false);
+  const [selectedEmoji, setSelectedEmoji] = useState("");
+
+  const handleEmojiClick = (emojiData: any) => {
+    setSelectedEmoji(emojiData.emoji);
+    setShowPicker(false);
+    setMessage(message + emojiData.emoji);
+    setSelectedEmoji("");
+  };
   const { sendMessage } = useConversation();
 
   const handleSend = async () => {
@@ -15,18 +31,34 @@ export default function ConversationInputView() {
     setMessage("");
   };
 
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSend();
+    }
+  };
+
   return (
     <div className="w-full border-t-2 flex flex-row gap-x-2">
       <Input
         placeholder="Type a message"
-        className="w-full h-[50px] border-none cursor-pointer"
+        className="w-full h-[50px] border-none cursor-pointer focus-visible:ring-0 focus-visible:ring-offset-0"
         type="text"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
 
       <div className="flex flex-row gap-x-4 items-center">
-        <SmileIcon className="size-6 cursor-pointer" />
+        <DropdownMenu open={showPicker} onOpenChange={setShowPicker}>
+          <DropdownMenuTrigger>
+            <SmileIcon />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <EmojiPicker onEmojiClick={handleEmojiClick} />
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <SendIcon
           className="size-6 cursor-pointer"
           onClick={() => handleSend()}
